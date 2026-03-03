c30fc039b4ab87e91c75abe300117da988fe939d
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2026-03-03 11:22:06 +0100

    Theme Selector

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import ThemeSelector from "@/components/shared/ThemeSelector";
+
 export default function Home() {
   return (
     <main className="w-full bg-background">
       hola
+      <ThemeSelector/>
     </main>
   );
 }
