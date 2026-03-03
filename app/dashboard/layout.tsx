5f49c5fcf9cf776eb9bdf7c4dc53c42e9295c69d
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2026-03-03 13:32:58 +0100

    responsive custom sidebar

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -5,18 +5,18 @@ import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }){
   return (
-    <div className="w-full min-h-screen flex flex-row">
+    <div className="w-full min-h-screen flex flex-col-reverse md:flex-row">
 
-      <div className="w-1/4 min-h-screen flex flex-col items-start justify-center gap-3 p-5">
-        <div className="self-end">
+      <div className="w-full md:w-1/4 md:min-h-screen flex md:flex-col items-center md:items-start justify-center gap-3 p-5">
+        <div className="md:self-end">
           <ThemeSelector />
         </div>
         
         <CustomLink href="/" styles="flex gap-1">
-          <Home/> Home
+          <Home/> <span className="sr-only md:not-sr-only">Home</span>
         </CustomLink>
         <CustomLink href="/dashboard/list" styles="flex gap-1">
-          <List /> Poke List
+          <List /> <span className="sr-only md:not-sr-only">Poke list</span>
         </CustomLink>
       </div>
 
