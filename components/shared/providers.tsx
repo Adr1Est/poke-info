4759bf83ca54cbf87538d30bd0cfde5e21bf7012
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2026-03-04 10:15:41 +0100

    change file locations

--- components/shared/providers.tsx
+++ components/shared/providers.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { QueryClientProvider } from "@tanstack/react-query"
 import { ReactNode } from "react"
-import { getQueryClient } from "@/app/get-query-client"
+import { getQueryClient } from "@/lib/get-query-client"
 import { ThemeProvider } from "next-themes"
 
 export default function Providers({ children }: { children: ReactNode }){
