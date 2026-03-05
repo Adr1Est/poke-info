13f986556837d187ace0686376572fd9ba6f167e
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2026-03-05 12:05:13 +0100

    FindGrid

--- app/dashboard/find/page.tsx
+++ app/dashboard/find/page.tsx
@@ -15,6 +15,5 @@ export default async function Find(){
     <HydrationBoundary state={dehydrate(queryClient)}>
       <FindGrid />  
     </HydrationBoundary>
-      
   )
 }
--- components/dashboard/FindGrid.tsx
+++ components/dashboard/FindGrid.tsx
@@ -1,10 +1,46 @@
+"use client"
 import CustomInput from "@/components/dashboard/CustomInput"
+import { getAllPokemon } from "@/services/getPokemonList"
+import { PokemonFromList } from "@/types/pokeTypes"
+import { useQuery } from "@tanstack/react-query"
+import { LoaderCircle, TriangleAlert } from "lucide-react"
+import PokeCardGrid from "./PokeCardGrid"
 
 export default function FindGrid(){
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["allPokes"],
+    queryFn: getAllPokemon,
+  })
+
+  if(isLoading){
+    return (
+      <div className="relative flex flex-row items-center justify-center w-full md:w-9/10 gap-1">
+       <LoaderCircle /> 
+       <h2>Loading... </h2>
+      </div>
+    )
+  }
+
+  if(error){
+    return (
+      <div className="relative flex flex-row items-center justify-center w-full md:w-9/10 gap-1">
+       <TriangleAlert /> 
+       <h2>Error fetching list</h2>
+      </div>
+    )
+  }
+
   return(
     <div className="relative flex flex-col items-center justify-center w-full p-2 md:p-0 md:w-9/10 gap-1">
+      <h1 className="font-semibold text-xl">{`${data.count} Pokemon`}</h1>
       <CustomInput />
-      <p>/find</p>
+      <div className="w-full grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-2 overflow-y-auto h-100 md:h-150">
+        {
+          data.results.map((p: PokemonFromList) => (
+            <PokeCardGrid key={p.name} name={p.name}/>
+          ))
+        }
+      </div>
     </div>
   )
 }
