e5ffccee744175e2423d875d1628e85290d225db
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2026-03-05 13:03:43 +0100

    dashboard start page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,39 @@
+import Link from "next/link"
+
 export default function Dashboard(){
+  const appRoutes = [
+    {
+      id: 1,
+      name: "Poke List",
+      route: "/dashboard/list"
+    },
+    {
+      id: 2,
+      name: "Poke finder",
+      route: "/dashboard/find"
+    }
+  ]
+
   return (
-    <div className="relative flex flex-col items-center justify-center w-full md:w-9/10 gap-1">
-      <p>/dashboard</p>
-    </div>
+    <section className="relative flex flex-col items-center md:items-start justify-center w-full md:w-9/10 gap-3">
+      <h1 className="font-semibold text-4xl">Welcome to Poke Info</h1>
+
+      <div className="flex flex-col gap-2 ml-5">
+        <h2 className="font-medium text-3xl">Navigation</h2>
+        <ul className="list-disc list-inside">
+          {
+            appRoutes.map((item) => (
+              <li key={item.id}>
+                <Link
+                  href={item.route}
+                >
+                  {item.name}
+                </Link>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    </section>
   )
 }
