81fcc327334af2cd57f4e1de80008c5d8e8666c4
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2026-03-05 11:05:40 +0100

    pokemon info and types

--- app/dashboard/list/[pokemon]/page.tsx
+++ app/dashboard/list/[pokemon]/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { capitalize } from "@/lib/capitalize"
 import { getPokeInfo } from "@/services/getPokemonList"
+import { PokeMoves, PokeStats } from "@/types/pokeTypes"
 import { useQuery } from "@tanstack/react-query"
 import { LoaderCircle, TriangleAlert } from "lucide-react"
 import Image from "next/image"
@@ -34,16 +35,45 @@ export default function PokemonCard(){
   }
 
   return(
-    <div>
-      <div>
-        <Image
-          src={data.sprites.other["official-artwork"].front_default} 
-          alt={`Picture of the pokemon ${data.species.name}`} 
-          width={20}
-          height={20}
-        />
+    <div className="w-full md:w-1/2 flex flex-col gap-3 h-150 overflow-y-auto">
+
+      <div className="flex flex-col md:flex-row items-center gap-5">
+        <div className="relative w-40 h-40 aspect-square">
+          <Image
+            src={data.sprites.other["official-artwork"].front_default} 
+            alt={`Picture of the pokemon ${data.species.name}`} 
+            fill
+          />
+        </div>
+        <h1 className="text-2xl font-semibold">{capitalize(data.species.name)}</h1>
       </div>
-      <h1>{capitalize(data.species.name)}</h1>
+      <div className="flex flex-col gap-1 items-center md:items-start">
+        <h2 className="text-xl font-medium">Stats</h2>
+        <div className="grid grid-cols-2 gap-3">
+          {
+            data.stats.map((s: PokeStats) => (
+              <p key={s.base_stat + s.stat.name}>
+                {`${capitalize(s.stat.name)}: ${s.base_stat}`}
+              </p>
+            ))
+          }
+        </div>
+      </div>
+      <div className="flex flex-col gap-1 items-center md:items-start">
+          <h3 className="text-lg font-medium">{`Weight: ${data.weight}`}</h3>
+      </div>
+      <div className="flex flex-col gap-1 items-center md:items-start">
+          <h2 className="text-xl font-medium">Moves</h2>
+          <div className="grid grid-cols-3 gap-3 overflow-y-auto h-50">
+            {
+              data.moves.map((m: PokeMoves) => (
+                <div key={m.move.name}>
+                  <h3>{m.move.name}</h3>
+                </div>
+              ))
+            }
+          </div>
+      </div>
     </div>
   )
 }
--- types/pokeTypes.ts
+++ types/pokeTypes.ts
@@ -2,3 +2,33 @@ export interface PokemonFromList {
   name: string;
   url: string;
 }
+
+export interface PokeStats {
+  base_stat: number;
+  effort: number;
+  stat: {
+    name: string;
+    url: string;
+  }
+}
+
+export interface PokeMoves {
+  move: {
+    name: string;
+    url: string;
+  };
+  version_group_details: MoveDetails[];
+}
+
+export interface MoveDetails {
+  level_learned_at: number;
+  move_learn_method: {
+    name: string;
+    url: string;
+  };
+  order?: string | null;
+  version_group: {
+    name: string;
+    url: string;
+  };
+}
